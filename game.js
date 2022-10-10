93e0819f62bea111a85c7f08a275fd35fe30644d
obre-bjorn <obre.bjorn@gmail.com>
2022-10-10 03:27:13 -0700

    add score functionality

--- game.js
+++ game.js
@@ -1,30 +1,58 @@
 //Choices 
 let choices = ['rock','paper','scissors']
+//Player choice
+
 // Computer's choice Between Rock,Paper and Scissors.
 let getComputerChoice =(choices)=>{
     return choices[Math.floor((Math.random()* choices.length))]
-
+    
 }
 
-let playerSelection = "rock"
+
+
+//Computer random choice
+
+//Player scores
+let compScore = 0;
+let playerScore = 0;
+
+// Rounds of games to play
+let rounds = 5
+
 
 function playround(playerSelection, computerSelection){
-    console.log(playerSelection);
-    console.log(computerSelection);
+    console.log( 'Player: ' + playerSelection);
+    console.log('Computer: ' + computerSelection);
+
     if (playerSelection.toLowerCase() == computerSelection ){
         return "It's a Tie"
     }else{
         if ((playerSelection.toLowerCase() == 'rock' && computerSelection =='scissors')||
         (playerSelection.toLowerCase() == 'scissors' && computerSelection =='paper')||
         (playerSelection.toLowerCase() == 'paper' && computerSelection =='rock')){
-            return 'Player has won'
+            playerScore++
+            rounds--
+            return 'You won!!!!'
         }else{
-            return 'You lose'
+            compScore++
+            rounds--
+            return 'You lose!!!!'
         }
-    }
+    } 
 }
 
 
-let computerSelection = getComputerChoice(choices)
+// Main game loop
+function game(){
+    while (rounds > 0){
+        let playerSelection = prompt('Enter ROCK, PAPER or SCISSORS: ');
+        let computerSelection = getComputerChoice(choices)
+        playround(playerSelection,computerSelection)
+        console.log('Player Score: ' + playerScore + '\n' + 'Computer Score: ' + compScore +'\n'
+        +'Rounds remaining: ' + rounds );
+    }
+    return `${playerScore > compScore?'You Won. YaY!': 'You lost to pc. LOL!'}`
+}
 
-console.log(playround(playerSelection,computerSelection))
+
+console.log(game());
